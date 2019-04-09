bb2165b6dbee8fc57140c5ed4672ba5570c778bf
Muhammad Suardi <suardihaidar22@gmail.com>
2019-04-09 21:07:07 +0700

    Restructure File

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ import {
 } from 'react-navigation';
 
 import Login from './views/Login'
-import Dashboard from './views/Dashboard'
+import Dashboard from './views/Dashboard/Dashboard'
 
 const appNavigator = createStackNavigator({
     login : { screen : Login, navigationOptions:{header:null}},
